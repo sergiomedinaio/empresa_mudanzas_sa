8d9dc64de1d9bc59bf4d8de85db32ca3490c1bda
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-11-14 15:16:50 -0300

    feat: add some Transportable objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
+        Transportable chair = new Transportable(
+            new Dimension(0.4,1.2,0.4),
+            "Fragile chair",
+            2.0,
+            true
+        );
+        Transportable matalChair = new Transportable(
+                new Dimension(0.4,1.2,0.4),
+                "Metal chair",
+                4.0,
+                false
+        );
+        Transportable oldTv = new Transportable(
+                new Dimension(1.4,1.2,0.6),
+                "Old TV",
+                4.0,
+                false
+        );
+        Transportable newTv = new Transportable(
+                new Dimension(1.4,1.2,0.1),
+                "New TV",
+                3.0,
+                true
+        );
+
+        Transportable myTv = new Transportable(
+                new Dimension(1.0,0.6,0.2),
+                "My TV",
+                2.5,
+                false
+        );
+
 
     }
 }
