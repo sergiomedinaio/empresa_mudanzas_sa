9651a0a8b4eec4f3946e982ec5b9fa3bfa089e9b
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-11-14 14:48:58 -0300

    feat: add basic project structure

---
+++ src/Dimension.java
@@ -0,0 +1,35 @@
+public class Dimension {
+    private double width;
+    private double height;
+    private double depth;
+
+    public Dimension(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+}
---
+++ src/Transportable.java
@@ -0,0 +1,2 @@
+public class Transportable {
+}
